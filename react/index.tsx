da0f2c09a05ff0637d24811cf3c1759ab1908b72
Yago Senhorini <yago.senhorini@enext.com.br>
2020-01-16 14:53:46 -0200

    feat: logs para produto

--- react/index.tsx
+++ react/index.tsx
@@ -3,8 +3,26 @@ import { canUseDOM } from 'vtex.render-runtime'
 import { PixelMessage } from './typings/events'
 
 export function handleEvents(e: PixelMessage) {
-  if(e.data.eventName){
-    console.log('Teste', e);
+  switch (e.data.event) {
+    case 'pageInfo': {
+      console.log(e.data);
+      break;
+    }
+    case 'addToCart': {
+      console.log(e.data);
+      break;
+    }
+    case 'productView': {
+      console.log('produto', e.data);
+      break;
+    }
+    case 'addToCart': {
+      console.log('add carrinho', e.data);
+      break;
+    }
+    default: {
+      return
+    }
   }
 }
 
